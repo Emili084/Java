b76f20ae4b96bd7400ef8662399ceffd74d77b0f
Emili084 <128594426+Emili084@users.noreply.github.com>
2023-07-03 13:33:21 +0200

    exo

---
+++ Tableaux/Movies.java
@@ -0,0 +1,16 @@
+class Movies {
+    public static void main(String[] args) {
+        String[] movies = {"Indiana Jones et les Aventuriers de l'arche perdue", "Indiana Jones et le Temple maudit", "Indiana Jones et la Dernière Croisade", "Indiana Jones et le Royaume du crâne de cristal"};
+        String[][] actors = {{"Harrison Ford", "Karen Allen", "Paul Freeman"}, {"Harrison Ford", "Kate Capshaw", "Jonathan Ke Quan"}, {"Harrison Ford", "Sean Connery", "Denholm Elliott"}, {"Harrison Ford", "Cate Blanchett", "Shia LaBeouf"}};
+
+        for (int i=0; i<movies.length; i++) {
+
+            String title = movies[i];
+            String[] movieActor = actors[i];
+            System.out.println("Dans le film " + title + ", les principaux acteurs sont :");
+            for (String actor : movieActor) {
+                System.out.println("- " + actor);
+            }
+        }
+    }
+}
---
+++ Variables/Variables.java
@@ -0,0 +1,13 @@
+class Variables {
+    public static void main(String[] args) {
+        String filmName = "Indiana Jones and the Raiders of the Lost Ark";
+        boolean isViewed = true;
+        int year = 1981;
+        double rating = 8.5;
+
+        System.out.println("Film: " + filmName);
+        System.out.println("Year: " + year);
+        System.out.println("Rating: " + rating);
+        System.out.println("Viewed: " + isViewed);
+    }
+}
